5a63aeffc2f4e2f396a8d2f62ddac14bcadf6974
Erick Fraga <efragafloresd@gmail.com>
2024-08-13 06:25:30 -0600

    [ADD] basket: create basket microservice with its operations

---
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -0,0 +1,14 @@
+﻿global using Carter;
+global using FluentValidation;
+global using Marten;
+global using Mapster;
+global using MediatR;
+
+global using Basket.API.Exceptions;
+global using Basket.API.Data;
+global using Basket.API.Models;
+
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,33 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to the container
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opt.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+// configure HTTP request pipeline
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.Run();
