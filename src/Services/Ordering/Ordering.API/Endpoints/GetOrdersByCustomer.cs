47a08e851f78afad6062bc1a318e66223f4adc6a
Erick Fraga <efragafloresd@gmail.com>
2024-08-16 07:09:57 -0600

    [IMP] ordering: add endpoints to API

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -0,0 +1,26 @@
+﻿using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+namespace Ordering.API.Endpoints;
+
+//public record GetOrderByCustomerRequest(string CustomerId);
+
+public record GetOrderByCustomerResponse(IEnumerable<OrderDTO> Orders);
+
+public class GetOrdersByCustomer : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+            var response = result.Adapt<GetOrderByCustomerResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByCustomer")
+        .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders By Customer")
+        .WithDescription("Get Orders By Customer");
+    }
+}
---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -0,0 +1,26 @@
+﻿using Ordering.Application.Orders.Queries.GetOrdersByName;
+
+namespace Ordering.API.Endpoints;
+
+//public record GetOrderByNameRequest(string Name);
+
+public record GetOrderByNameResponse(IEnumerable<OrderDTO> Orders);
+
+public class GetOrdersByName : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+            var response = result.Adapt<GetOrderByNameResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByName")
+        .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders By Name")
+        .WithDescription("Get Orders By Name");
+    }
+}
